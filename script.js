b7e721de0d391c058261300023405faaf8ee1319
Mihaela <mihaelavalac@yahoo.com>
2021-01-13 14:56:06 -0500

    Created the ingredients list.

--- script.js
+++ script.js
@@ -1,49 +1,3 @@
-
-
-
-function generateFood(userLetter) {
-  var foodUrl =
-  "http://www.themealdb.com/api/json/v1/1/search.php?f=" + userLetter;
-
-  $.ajax({
-    url: foodUrl,
-    method: "GET",
-  }).then(function (data) {
-    var randNumber = generateNumber(data.meals.length);
-    var recipeGenerator = displayFoodSection (
-      data.meals[randNumber].strMeal,
-      data.meals[randNumber].strMealThumb,
-      data.meals[randNumber].strInstructions,
-    );
-    $("#food-box").append(recipeGenerator); 
-  });
-};
- 
-function generateNumber(number) {
-  return Math.floor(Math.random() *  number);
-}
-
-function displayFoodSection (foodName, foodImg, foodInstructions) {
-    return ` <h3 id= "choice-name">${foodName}</h3>
-    <img id= "choice-image" src="${foodImg}">
-    <div id = "instructions">Cooking Instruction: ${foodInstructions}</div>`
-}
-
-
-function generateCocktail(userLetter) {
-  var drinksUrl =
-  " http://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + userLetter;
-  $.ajax({
-    url: drinksUrl,
-    method: "GET",
-  }).then(function (response) {
-  });
-}
-
-
-
-// This for loop generates buttons with letters.
-
 var html = " ";
 var c;
 for (var i = 65; 90 >= i; i++) {
@@ -55,9 +9,72 @@ document.getElementById("box").innerHTML = html;
 
 $(".letter").click(function (event) {
   var letter = event.target.innerText.toLowerCase();
-  localStorage.setItem("userLetter", "letter");
-  console.log(letter);
+  // localStorage.setItem("userLetter", "letter");
+  // console.log(letter);
   generateCocktail(letter);
   generateFood(letter);
-
 });
+
+function generateFood(userLetter) {
+  var foodUrl =
+  "http://www.themealdb.com/api/json/v1/1/search.php?f=" + userLetter;
+
+  $.ajax({
+    url: foodUrl,
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
+    var randNumber = generateNumber(data.meals.length);
+    console.log(randNumber);
+    var ingPath = data.meals[randNumber];
+    var array = [];
+       for (let i = 1; i < 15; i++) {
+        var ingredient = "strIngredient" + i;
+        var measurement = "strMeasure" + i;
+
+        if (ingPath[ingredient] === "") {
+          break;
+        }
+         array.push(
+          ingPath[ingredient] + " : " + ingPath[measurement]
+        );
+      }
+      console.log(array);
+      //generateIngredients(array);
+    var recipeGenerator = generateSelectedSection (
+      data.meals[randNumber].strMeal,
+      data.meals[randNumber].strMealThumb,
+      array,
+      data.meals[randNumber].strInstructions,
+    );
+    $("#food-box").append(recipeGenerator); 
+  });
+};
+function generateNumber(number) {
+  return Math.floor(Math.random() *  number);
+}
+function generateIngredients() {
+  $("#ingredients").empty();
+  for (var i =0; i<array.length; i++) {
+    var recipeIngredients = $("<li>" + array[i] + "</li>");
+    $("#ingredients").append(recipeIngredients);
+  }
+}
+
+function generateSelectedSection (name, img, ingredients, instructions) {
+    return ` <h3 id= "choice-name">${name}</h3>
+    <img id= "choice-image" src="${img}">
+    <ul id="ingredients-list">Ingredients: ${ingredients}</ul>
+    <div id = "instructions">Cooking Instruction: ${instructions}</div>`
+}
+
+function generateCocktail(userLetter) {
+  var drinksUrl =
+  " http://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + userLetter;
+  $.ajax({
+    url: drinksUrl,
+    method: "GET",
+  }).then(function (response) {
+  });
+}
+
